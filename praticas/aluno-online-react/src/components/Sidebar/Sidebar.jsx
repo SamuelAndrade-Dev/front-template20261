6f96e1e508c20625183432e0535980fbe15ceb9a
samuel.pedro <samuelandrade.spa@gmail.com>
2026-05-11 20:11:17 -0300

    feat: conclui pratica06. Fecha #06

--- praticas/aluno-online-react/src/components/Sidebar/Sidebar.jsx
+++ praticas/aluno-online-react/src/components/Sidebar/Sidebar.jsx
@@ -1,37 +1,22 @@
 import "./Sidebar.css";
-import SidebarItem from "./SidebarItem/SidebarItem";
-import learn from "../../assets/learn.svg";
+import learn from "../../assets/learn.svg"
+import { NavLink, Link } from "react-router";
 
-function Sidebar({ mudarTela }) {
-  const itens = [
-    { texto: "Dashboard", rota: "dashboard" },
-    { texto: "Notas", rota: "notas" },
-    { texto: "Faltas", rota: "faltas" },
-    { texto: "Boletos", rota: "boletos" },
-    { texto: "Requerimentos", rota: "requerimentos" },
-    { texto: "Sair", rota: "sair" }
-  ];
-
+function Sidebar() {
   return (
     <aside className="sidebar">
       <header className="sidebar-brand">
         <img src={learn} alt="Foto do chapéu de formatura" />
         <h2>Aluno Online</h2>
       </header>
-
-      <nav aria-label="Navegação lateral">
+      <nav>
         <ul>
-          {itens.map((item) => (
-            <SidebarItem
-              key={item.texto}
-              texto={item.texto}
-              acao={() =>
-                item.rota === "sair"
-                  ? console.log("Sair")
-                  : mudarTela(item.rota)
-              }
-            />
-          ))}
+          <li><NavLink to="/" className={({ isActive }) => isActive ? 'active' : ''}>Dashboard</NavLink></li>
+          <li><NavLink to="/boletos" className={({ isActive }) => isActive ? 'active' : ''}>Boletos</NavLink></li>
+          <li><NavLink to="/faltas" className={({ isActive }) => isActive ? 'active' : ''}>Faltas</NavLink></li>
+          <li><NavLink to="/notas" className={({ isActive }) => isActive ? 'active' : ''}>Notas</NavLink></li>
+          <li><NavLink to="/requerimentos" className={({ isActive }) => isActive ? 'active' : ''}>Requerimentos</NavLink></li>
+          <li><Link to="/login">Sair</Link></li>
         </ul>
       </nav>
     </aside>
