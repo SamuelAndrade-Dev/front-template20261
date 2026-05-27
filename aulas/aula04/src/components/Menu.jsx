8139e325b5c0a8d954fbe931c6de873c1df153c7
samuel.pedro <samuelandrade.spa@gmail.com>
2026-05-27 19:22:25 -0300

    fix: merge

--- aulas/aula04/src/components/Menu.jsx
+++ aulas/aula04/src/components/Menu.jsx
@@ -5,13 +5,13 @@ function Menu() {
     const usuarioId = 0; //pegou da API
     return (
         <nav>
-            <lu>
+            <ul>
                 <li><NavLink to="/">Página Home</NavLink></li>
                 <li><NavLink to={`/perfil/${usuarioId}`}>Perfil do usuário</NavLink></li>
                 <li><NavLink to="/settings">Configurações</NavLink></li>
                 <li><NavLink to="/about">Sobre</NavLink></li>
                 <li><Link to="/login">Sair</Link></li>
-            </lu>
+            </ul>
         </nav>
     )
 }
--- aulas/aula04/src/pages/Perfil.jsx
+++ aulas/aula04/src/pages/Perfil.jsx
@@ -1,9 +1,53 @@
 import { useParams } from "react-router";
+import { useForm } from "react-hook-form"
 
 function Perfil() {
     const { id } = useParams();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const salvar = (data) => console.log(data)
+
+    //register() => {name="", onChange"", onBlur=""}
+    // handleSubmit() => dispara a validacao, 
+    //informa ao React que mudou os campos
+
+    const regras = {
+        nome: { required: "Nome é obrigatório" },
+        email: { required: "E-mail é obrigatório" },
+        nascimento: {},
+        telefone: {}
+    }
+
     return <>
         <h1>Perfil do usuário {id}</h1>
+        <form onSubmit={handleSubmit(salvar)}>
+            <div>
+                <label htmlFor="nome">Nome</label>
+                <input type="text"
+                    id="nome"
+                    {...register("nome", regras.nome)} />
+                {errors?.nome && <p>{errors.nome.message}</p>}
+            </div>
+            <div>
+                <label htmlFor="email">E-mail</label>
+                <input type="email"
+                    id="email"
+                    {...register("email", regras.email)} />
+                {errors?.email && <p>{errors.email.message} </p>}
+            </div>
+            <div>
+                <label htmlFor="nascimento">Nascimento</label>
+                <input type="date"
+                    id="data"
+                    {...register("nascimento", regras.nascimento)} />
+            </div>
+            <div>
+                <label htmlFor="telefone">Telefone</label>
+                <input type="number"
+                    id="telefone"
+                    {...register("telefone", regras.telefone)} />
+            </div>
+            <button type="submit">Salvar</button>
+        </form>
     </>
 }
 
