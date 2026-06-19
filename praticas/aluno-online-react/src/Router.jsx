4c2286af580db5f1b748d226462c7784508f1109
Samuel-Andrade <samuelandrade.spa@gmail.com>
2026-06-18 22:22:03 -0300

    feat: conclui pratica08. Fecha #08

--- praticas/aluno-online-react/src/Router.jsx
+++ praticas/aluno-online-react/src/Router.jsx
@@ -1,47 +1,22 @@
-<<<<<<< HEAD
 import { useEffect, useState } from 'react';
-=======
-import { useEffect } from 'react';
->>>>>>> origin/develop
 
 import AppSite from './AppSite';
 import Login from './pages/Login/Login';
 import { useAuth } from './contexts/useAuth.js';
 
-
 export default function Router() {
-<<<<<<< HEAD
   const [path, setPath] = useState(window.location.pathname);
   const { autenticado } = useAuth();
 
   useEffect(() => {
     const onPopState = () => setPath(window.location.pathname);
-=======
-  const { autenticado } = useAuth();
-
-  useEffect(() => {
-    const onPopState = () => {
-      window.location.reload();
-    };
->>>>>>> origin/develop
     window.addEventListener('popstate', onPopState);
     return () => window.removeEventListener('popstate', onPopState);
   }, []);
 
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/develop
   if (!autenticado) {
     return <Login />;
   }
 
-<<<<<<< HEAD
   return <AppSite currentPath={path} onPathChange={setPath} />;
-=======
-  return <AppSite />;
->>>>>>> origin/develop
 }
-
-
-
